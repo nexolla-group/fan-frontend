a4197ce0d2d2614146c1e48d58ab9891928e1445
Joel Kwihangana <joelkwijoe@gmail.com>
2023-02-01 13:16:08 +0200

    userProfile fix

--- src/pages/UserProfiles/UserProfile.jsx
+++ src/pages/UserProfiles/UserProfile.jsx
@@ -2,39 +2,111 @@ import React, { useState } from "react";
 import { Navbar } from "../../Home/Components";
 import "./userProfile.scss";
 import userProfile from "../../assets/userprofile.png";
+import SaveOutlinedIcon from "@mui/icons-material/SaveOutlined";
+import { Link } from "react-router-dom";
 
 const UserProfile = () => {
-  const [isOpen, setIsOpen] = useState("");
   return (
     <>
       <Navbar />
-      <div className="grid-container">
-        <div className="left-grid">
-          {" "}
-          <div className="user-profile">
-            <img src={userProfile} alt="User Profile" />
-            <h3>Username</h3>
-            <p>user@example.com</p>
+      <div className="container-fluid ProfileContainer">
+        <div className="user-page">
+          <div className="user-header">
+            <div className="user-photo">
+              <img src="https://via.placeholder.com/150x150" alt="User Photo" />
+            </div>
+            <div className="user-info">
+              <h2>User Name</h2>
+              <p>Email: user@email.com</p>
+              <p>Location: City, Country</p>
+
+              <Link to="/logout">
+                <p
+                  className="text-danger mt-5 fw-bold cur-pointer"
+                  style={{ cursor: "pointer" }}
+                  onClick={(e) => {
+                    if (!window.confirm("Are you sure you want to log out?")) {
+                      e.preventDefault();
+                    }
+                  }}
+                >
+                  Logout
+                </p>
+              </Link>
+            </div>
           </div>
-          <div className="accordion">
-            <button onClick={() => setIsOpen(!isOpen)}>Links</button>
-            {isOpen && (
+          <div className="update-user-details">
+            <div className="user-basic-info">
               <ul>
                 <li>
-                  <a href="#">Link 1</a>
+                  Full Name
+                  <input
+                    type="text"
+                    name=""
+                    id=""
+                    placeholder="Your Full name"
+                  />
                 </li>
                 <li>
-                  <a href="#">Link 2</a>
+                  Username
+                  <input
+                    type="text"
+                    name=""
+                    id=""
+                    placeholder="Your Username"
+                  />
                 </li>
                 <li>
-                  <a href="#">Link 3</a>
+                  Email
+                  <input type="text" name="" id="" placeholder="Your Email" />
                 </li>
+                <li>
+                  Phone
+                  <input
+                    type="text"
+                    name=""
+                    id=""
+                    placeholder="Your Phone number"
+                  />
+                </li>
+
+                <li>
+                  Address
+                  <input type="text" name="" id="" placeholder="Your Address" />
+                </li>
+                <button className="btn btn-outline-primary">
+                  <SaveOutlinedIcon /> Save Profile
+                </button>
               </ul>
-            )}
+            </div>
+            <div className="update-password">
+              <p>Update Password</p>
+              <ul>
+                <li>
+                  Current password
+                  <input
+                    type="password"
+                    name=""
+                    id=""
+                    placeholder="Enter Existing password"
+                  />
+                </li>
+                <li>
+                  New Password
+                  <input
+                    type="password"
+                    name=""
+                    id=""
+                    placeholder="Enter New password"
+                  />
+                </li>
+                <button className="btn btn-outline-primary">
+                  <SaveOutlinedIcon /> Save Password
+                </button>
+              </ul>
+            </div>
           </div>
         </div>
-        <div className="center-grid">Center Grid</div>
-        <div className="right-grid">Right Grid</div>
       </div>
     </>
   );
