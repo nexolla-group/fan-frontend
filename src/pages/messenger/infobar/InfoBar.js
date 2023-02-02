ba365ea38ae7a5fc5b10766b18a5d893f8a2850f
Joel Kwihangana <joelkwijoe@gmail.com>
2023-02-02 10:36:33 +0200

    fix src/Home/Components/navbar/Navbar.jsx, src/Home/Components/navbar/navbar.css, src/pages/messenger/AvairableGroups/AvairableGroups.jsx (useToolTips, src/pages/messenger/infobar/InfoBar.js, src/pages/register/Register.js (using validations,src/pages/register/Register.js))

--- src/pages/messenger/infobar/InfoBar.js
+++ src/pages/messenger/infobar/InfoBar.js
@@ -5,17 +5,15 @@ import { Link } from "react-router-dom";
 
 const InfoBar = ({ selectedGroup }) => {
   return (
-    <div className='infoBar'>
-      <div className='leftInnerContainer'>
-        <div>
-          <h3>{selectedGroup?.groupDetails?.groupName}</h3>
-          <p className='groupDsc'>{selectedGroup?.groupDetails?.description}</p>
-        </div>
-      </div>
-      <div className='rightInnerContainer'>
+    <div className="infoBar">
+      <div className="heading">
+        <h3>{selectedGroup?.groupDetails?.groupName}</h3>
         <Link to={"/contributions/" + selectedGroup?.groupDetails?._id}>
           Contribute
         </Link>
+      </div>
+      <div className="groupDesciption">
+        <p>{selectedGroup?.groupDetails?.description}</p>
       </div>
     </div>
   );
