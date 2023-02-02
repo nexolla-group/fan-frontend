a4922fb0f51f29aacf61001445109b65128699c6
Jean Claude <demo.claude15@gmail.com>
2023-02-02 03:50:02 +0200

    add and delete post

--- src/admin/pages/Posts.jsx
+++ src/admin/pages/Posts.jsx
@@ -4,8 +4,15 @@ import Sidebar from "../components/sidebar/Sidebar";
 import "./posts.css";
 
 import Fab from "@mui/material/Fab";
-import { TextField } from "@mui/material";
-import { makeStyles } from "@material-ui/core";
+import { Box, TableContainer, TextField } from "@mui/material";
+import { makeStyles, Paper } from "@material-ui/core";
+import axios from "axios";
+import { useSelector } from "react-redux";
+import { errorHandler, toastMessage } from "../../helpers";
+import { DataGrid } from "@mui/x-data-grid";
+import { useMemo } from "react";
+import { useEffect } from "react";
+import { Delete, Edit } from "@mui/icons-material";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -16,14 +23,96 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 const Posts = () => {
+  const { token } = useSelector((state) => state.user);
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
+  const [postingBlog, setPostingBlog] = useState(false);
+  const [animation, setAnimation] = useState("");
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    // Add code to save the blog to the database
   };
+
+  const handlePost = (e) => {
+    e.preventDefault();
+    setPostingBlog(true);
+    axios
+      .post(process.env.REACT_APP_BACKEND_URL + "/api/post", {
+        title,
+        desc: content,
+        token,
+      })
+      .then((res) => {
+        setTitle("");
+        setContent("");
+        toastMessage("success", res.data.message);
+        fetchPost();
+        setPostingBlog(false);
+      })
+      .catch((error) => {
+        errorHandler(error);
+        setPostingBlog(false);
+      });
+  };
+
+  const [fetchedPost, setFetchedPost] = useState([]);
+  const fetchPost = () => {
+    axios
+      .get(process.env.REACT_APP_BACKEND_URL + "/api/post")
+      .then((res) => {
+        setFetchedPost(res.data.data);
+      })
+      .catch((error) => console.log("Bad", error));
+  };
+  useEffect(() => {
+    fetchPost();
+  }, []);
+
   const classes = useStyles();
+
+  const columns = useMemo(
+    () => [
+      { field: "title", headerName: "Title", width: 150 },
+      { field: "desc", headerName: "Content", width: 200 },
+      { field: "userId", headerName: "user", width: 200 },
+      {
+        field: "createdAt",
+        headerName: "Created At",
+        width: 100,
+        renderCell: (item) => {
+          return new Date(item.row.createdAt).toLocaleDateString();
+        },
+      },
+      {
+        field: "action",
+        headerName: "Action",
+        width: 80,
+        renderCell: (item) => (
+          <>
+            {" "}
+            <Edit /> <Delete onClick={() => handleDelete(item.row._id)} />
+          </>
+        ),
+      },
+    ],
+    []
+  );
+
+  const handleDelete = (id) => {
+    axios
+      .delete(
+        process.env.REACT_APP_BACKEND_URL +
+          "/api/post/" +
+          id +
+          "/?token=" +
+          token
+      )
+      .then((res) => {
+        fetchPost();
+        toastMessage("success", res.data.message);
+      })
+      .catch((error) => console.log(error));
+  };
   return (
     <div className='Home'>
       <Sidebar />
@@ -45,6 +134,8 @@ const Posts = () => {
                     label='Title'
                     variant='outlined'
                     placeholder='Enter Title'
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
                   />
                 </div>
                 <div>
@@ -55,6 +146,8 @@ const Posts = () => {
                     placeholder='Enter Content'
                     multiline
                     rows={4}
+                    value={content}
+                    onChange={(e) => setContent(e.target.value)}
                   />
                 </div>
                 <div>
@@ -63,8 +156,9 @@ const Posts = () => {
                     size='medium'
                     color='dark'
                     aria-label='add'
+                    onClick={handlePost}
                   >
-                    Create
+                    {postingBlog ? "Creating..." : "Create"}
                   </Fab>
                 </div>
               </form>
@@ -103,12 +197,28 @@ const Posts = () => {
                     size='medium'
                     color='dark'
                     aria-label='add'
+                    onClick={handleSubmit}
                   >
                     Create
                   </Fab>
                 </div>
               </form>
             </div>
+          </div>
+        </div>
+        <div className='row'>
+          <div className='col col-md-6 col-sm-12 col-12'>
+            <TableContainer component={Paper} className={`table ${animation}`}>
+              <Box sx={{ width: "100%" }}>
+                <div style={{ height: 600, width: "100%" }}>
+                  <DataGrid
+                    rows={fetchedPost}
+                    columns={columns}
+                    getRowId={(row) => row._id}
+                  />
+                </div>
+              </Box>
+            </TableContainer>
           </div>
         </div>
       </div>
